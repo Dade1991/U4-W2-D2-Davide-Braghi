02497c7604a3d09cc872cda9b0c1935ab0cfbd98
Dade1991 <dade91@msn.com>
2025-09-16 18:03:10 +0200

    Added solution's folder

--- src/Ex3.java
+++ src/Ex3.java
@@ -3,6 +3,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Ex3 {
+
     public static void Exercise3() {
 
         System.out.println("Esercizio 3 -------------------------------");
