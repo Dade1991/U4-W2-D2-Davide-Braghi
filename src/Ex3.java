a2ebc25a664327d294f6684f46370e755dcab087
Dade1991 <dade91@msn.com>
2025-09-16 16:56:10 +0200

    Started Ex3

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Ex3 {
@@ -6,5 +8,25 @@ public class Ex3 {
         System.out.println("Esercizio 3 -------------------------------");
 
         Scanner scannerTool = new Scanner(System.in);
+
+        Map<Integer, String> rubrica = new HashMap<>();
+
+        while (true) {
+            System.out.println("Vuoi inserire un nuovo Contatto? \n Digita 's' per SI oppure 'n' per NO.");
+            String outcome = scannerTool.nextLine().toLowerCase().trim();
+            if (outcome.equals("n")) {
+                break;
+            } else {
+                System.out.println("Inserisci il numero di Telefono:");
+                int phoneNumber = scannerTool.nextInt();
+                scannerTool.nextLine();
+                System.out.println("Inserisci il nome del Contatto:");
+                String contactName = scannerTool.nextLine();
+                rubrica.put(phoneNumber, contactName);
+            }
+        }
+
+        System.out.println("La tua rubrica: " + rubrica);
+
     }
 }
