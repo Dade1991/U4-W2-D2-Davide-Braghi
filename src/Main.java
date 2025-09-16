3d3d8520d2fed4dd4b3e6bfe06ef3f54ffdf57f4
Dade1991 <dade91@msn.com>
2025-09-16 16:56:38 +0200

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-//        Ex1.Exercise1();
-//        Ex2.Exercise2();
+        Ex1.Exercise1();
+        Ex2.Exercise2();
         Ex3.Exercise3();
 
     }
